3bf6470e77b8dcb5e2b639ca96114180f61205b5
HsnAkk <hasan.akkas@web.de>
2019-11-11 14:26:04 +0100

    ProfileBar part

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import ProfileBar from './components/ProfileBar'; 
 
 var Splash = ()=> { return null; }
-var ProfileBar = ()=> { return null; }
 var Navbar = ()=> { return null; }
 var Login = ()=> { return null; }
 var Signup = ()=> { return null; }
---
+++ src/components/ProfileBar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Navbar, NavDropdown } from 'react-bootstrap';
+import { IoMdLogIn } from 'react-icons/io';
+import { IoMdPerson } from 'react-icons/io';
+import { IoMdLogOut } from 'react-icons/io';
+
+
+class ProfileBar extends Component {
+    render() {
+        return (
+            <Navbar bg="light" className="justify-content-end mr-3">
+                <NavDropdown title="Login" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1"><IoMdLogIn className="mr-2"/>  Login</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2"><IoMdPerson className="mr-2"/> Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3"><IoMdLogOut className="mr-2"/>Logout</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar>
+        )
+    }
+}
+
+export default ProfileBar;
