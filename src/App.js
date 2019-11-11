16f2eb9afacd7509b65be98353513aa1cd827c70
Jovan Derebanoski <jd7@students.kiron.ngo>
2019-11-11 15:13:50 +0100

    Integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 import Navibar from "./components/Navibar";
 import ProfileBar from './components/ProfileBar';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import './App.css';
 
 var Splash = ()=> { return null; }
+var LandingPage = (props)=> { switch( props.page ){
+                            case "login" : 
+                              return <Home />
+                          
+                            case "signup" :
+                              return <Signup />
+                              
+                            default :
+                              return <Splash />
+
+  } 
+}
+
+
 var Home = ()=> { return null; }
 
 
-function App() {
-  return (
-    <div className="App">
-      <Splash />
-      <ProfileBar />
-      <Navibar />
-      <Login />
-      <Signup />
-      <Home />
+
+
+
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+
+      user : {
+        username : '',
+        password : ''
+      },
+
+      login : false,
+      page : "splash"
+    }
+
+    
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Container fluid>
+            
+                <Row>
+                  <Col md="10" sm="12">
+                    <Navibar />
+                    </Col>  
+                    <Col md="2" sm="12">
+                      <ProfileBar/>       
+                    </Col>
+               
+           
+     
+              <Col>
+                { this.state.login 
+                ?
+                  <Row><Col><Home /></Col></Row>
+                : 
+                  <Row><Col><LandingPage page={this.state.page} /></Col></Row>
+                }
+              </Col>
+            </Row> 
+      
+      
+        </Container>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
+
+
+
