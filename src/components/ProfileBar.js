4950c10f946610ef41c86965d6adf1a518e0edf9
Bora Manga <bora.mng525@gmail.com>
2019-11-12 12:16:36 +0100

    Profilebar updated

--- src/components/ProfileBar.js
+++ src/components/ProfileBar.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
-import { Navbar, NavDropdown } from 'react-bootstrap';
-import { IoMdLogIn, IoMdPerson, IoMdLogOut, IoIosPaper } from 'react-icons/io';
+import React, { Component } from "react";
+import { Navbar, NavDropdown } from "react-bootstrap";
+import { IoMdLogIn, IoMdPerson, IoMdLogOut, IoIosPaper } from "react-icons/io";
 
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 class ProfileBar extends Component {
-    render() {
-        return (
-            <Navbar bg="light" className="justify-content-end mr-3">
-                <NavDropdown title="Login" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/login"><IoMdLogIn className="mr-2"/>Login</NavDropdown.Item>
-                    <NavDropdown.Item href="/signup"><IoIosPaper className="mr-2"/>Signup</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2"><IoMdPerson className="mr-2"/>Profile</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3"><IoMdLogOut className="mr-2"/>Logout</NavDropdown.Item>
-                </NavDropdown>
-            </Navbar>
-        )
-    }
+  render() {
+    return (
+      <Navbar bg="light" className="justify-content-end mr-3">
+        {this.props.login ? (
+          <NavDropdown title="Welcome" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.2">
+              <IoMdPerson className="mr-2" />
+              Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">
+              <IoMdLogOut className="mr-2" />
+              Logout
+            </NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <NavDropdown title="Login" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/login">
+              <IoMdLogIn className="mr-2" />
+              Login
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/signup">
+              <IoIosPaper className="mr-2" />
+              Signup
+            </NavDropdown.Item>
+          </NavDropdown>
+        )}
+      </Navbar>
+    );
+  }
 }
 
 export default ProfileBar;
