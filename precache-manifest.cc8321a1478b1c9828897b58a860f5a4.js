6f0c236727644b01318eb1ec12d326da5c2c19ca
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-02-28 20:11:56 +0100

    Updates

---
+++ precache-manifest.cc8321a1478b1c9828897b58a860f5a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ba84b283e5323e43e8fae7fd9d7acf",
+    "url": "/overtime/index.html"
+  },
+  {
+    "revision": "b6c86148c22198a64cd7",
+    "url": "/overtime/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4fb79117cf69a8346b52",
+    "url": "/overtime/static/css/main.44d17d1d.chunk.css"
+  },
+  {
+    "revision": "b6c86148c22198a64cd7",
+    "url": "/overtime/static/js/2.4613f279.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/overtime/static/js/2.4613f279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb79117cf69a8346b52",
+    "url": "/overtime/static/js/main.2911a34c.chunk.js"
+  },
+  {
+    "revision": "5b99b0274886328cc47c",
+    "url": "/overtime/static/js/runtime-main.8c504ee4.js"
+  }
+]);
