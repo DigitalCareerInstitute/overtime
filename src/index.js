7706cb56441d28a35a835efdeca5fa73a25b3114
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-03-01 03:04:56 +0100

    refactor: split into files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
+import Overtime from './Overtime';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Overtime />, document.getElementById('root'));
 
 serviceWorker.register();
