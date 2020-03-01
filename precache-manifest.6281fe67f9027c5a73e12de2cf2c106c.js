0c915a2ff03d3cb36adfa8ab31cf38e069e09b63
anx <sebastian.glaser@digitalcareerinstitute.org>
2020-03-01 20:36:22 +0100

    Updates

---
+++ precache-manifest.6281fe67f9027c5a73e12de2cf2c106c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539b4566cc21cf1e20d2af65fe16f28a",
+    "url": "/overtime/index.html"
+  },
+  {
+    "revision": "c4bb90dfac31a33bfee1",
+    "url": "/overtime/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "5493ce6fb0412f99d7ca",
+    "url": "/overtime/static/css/main.00832ba1.chunk.css"
+  },
+  {
+    "revision": "c4bb90dfac31a33bfee1",
+    "url": "/overtime/static/js/2.997adfa2.chunk.js"
+  },
+  {
+    "revision": "50357fe5786a790fa927c899743daafb",
+    "url": "/overtime/static/js/2.997adfa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5493ce6fb0412f99d7ca",
+    "url": "/overtime/static/js/main.9ca01044.chunk.js"
+  },
+  {
+    "revision": "5b99b0274886328cc47c",
+    "url": "/overtime/static/js/runtime-main.8c504ee4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/overtime/precache-manifest.febdab5b6ca08303be80a910578fdf0b.js"
+  "/overtime/precache-manifest.6281fe67f9027c5a73e12de2cf2c106c.js"
 );
 
 self.addEventListener('message', (event) => {
